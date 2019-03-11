60363119a71ecfa53ca4be41e31d64f3f1cada7a
unknown <jonathanmohan.net@gmail.com>
2019-03-11 17:50:19 -0500

    jungle

---
+++ JungleOverloading/Employee.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JungleOverloading
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        static Random rnd = new Random();
+
+        static List<string> restaurants = new List<string>
+        {
+            "Cancun Mexican Restaurant",
+            "Hattie B's Hot Chicken",
+            "Fat Mo's Burgers & Fries",
+            "Pizza Perfect",
+
+        };
+
+        public int generateRandomNum()
+        {
+            // creates a number between 1 and the length of the list
+            int randNum = rnd.Next(1, restaurants.Count);
+            return randNum;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{FirstName} {LastName} is eating at {restaurants[generateRandomNum()]}.");
+            Console.ReadKey();
+        }
+
+        public void Eat(string food)
+        {
+            Console.WriteLine($"{FirstName} {LastName} ate {food} at the office.");
+            Console.ReadKey();
+        }
+
+        public void Eat(List<Employee> companions)
+        {
+            string foodies = "";
+            for (int i = 0; i < companions.Count; i++)
+            {
+                if (companions[i].FirstName != FirstName)
+                {
+                    if (i != companions.Count - 1)
+                    {
+                        foodies += companions[i].FirstName + ",";
+                    }
+                    else
+                    {
+                        foodies += "and " + companions[i].FirstName;
+                    }
+                }
+            }
+            Console.WriteLine($"{FirstName} is eating at {restaurants[generateRandomNum()]} with {foodies}.");
+            Console.ReadKey();
+        }
+
+        public void Eat(string food, List<Employee> companions)
+        {
+            string foodies = "";
+            for (int i = 0; i < companions.Count; i++)
+            {
+                if (companions[i].FirstName != FirstName)
+                {
+                    if (i != companions.Count - 1)
+                    {
+                        foodies += companions[i].FirstName + ",";
+                    }
+                    else
+                    {
+                        foodies += "and " + companions[i].FirstName;
+                    }
+                }
+            }
+            Console.WriteLine($"{FirstName} is eating {food} at {restaurants[generateRandomNum()]} with {foodies}.");
+            Console.ReadKey();
+        }
+
+        public Employee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ JungleOverloading/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace JungleOverloading
+{
+    class Program
+    {
+        public static List<Employee> Employees { get; set; } = new List<Employee>();
+
+        static void Main(string[] args)
+        {
+
+            Employee link = new Employee("Link", "Zelda");
+            Employee samus = new Employee("Samus", "Metroid");
+            Employee gannon = new Employee("Gannon", "Monster");
+            Employee bowser = new Employee("Bowser", "Koopa");
+
+            Employees.Add(link);
+            Employees.Add(samus);
+            Employees.Add(gannon);
+            Employees.Add(bowser);
+
+
+            gannon.Eat();
+            gannon.Eat("microwavable garbage");
+            gannon.Eat(Employees);
+            gannon.Eat("some good grub", Employees);
+
+        }
+    }
+}
